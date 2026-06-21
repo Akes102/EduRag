c8992fe77dabc23eeee1e18661230d7c31e5994a
Arnold Muller <arnold.muller@sabiogroup.com>
2026-06-21 18:11:20 +0200

    revert + upgrade

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 KNOWLEDGE_BASE = os.path.join(BASE_DIR, "knowledge-base")
-DB_PATH = os.path.join(BASE_DIR, "db")
+DB_PATH = "./db"
 
 EMBEDDING_MODEL = "all-MiniLM-L6-v2"
 
